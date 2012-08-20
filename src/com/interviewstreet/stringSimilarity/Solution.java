8e2f9b5a19726aeda1984d015cbda99feaf0ee63
Róbert Ormándi <ormandi.robert@gmail.com>
2012-08-20 22:12:00 +0200

    Amost ready

--- src/com/interviewstreet/stringSimilarity/Solution.java
+++ src/com/interviewstreet/stringSimilarity/Solution.java
@@ -18,27 +18,36 @@ public class Solution {
       while (text != null) {
         // process text
         final int length = text.length();
-        long sum = 0;
         
         // s[i] stores the length of the proper substring ended in i which matches the prefix of the text (KMP)
         int[] s = new int[length];
+        int[] z = new int[length];
+        
         // initialize the base case
         s[0] = 0;
-        
-        System.out.println(text + "\ns[" + 0 + "] = " + s[0]);
-        
+        z[0] = length;
         // similar processing than that was proposed in the KMP algorithm
         for (int i = 1; i < length; i ++) {
           int x = i - 1;
-          
           while (0 < x && text.charAt(s[x]) != text.charAt(i)) {
             x = s[x] - 1;
           }
           s[i] = ( 0 <= x && text.charAt(s[x]) == text.charAt(i)) ? s[x] + 1 : 0;
           
-          System.out.println("s[" + i + "] = " + s[i]);
+          // compute z-values
+          final int j = i - s[i] + 1;
+          if (0 <= j) {
+            z[j] = s[i];
+          }
+          System.out.println("s[" + i + "] = " + s[i] + ", z[" + j + "] = " + z[j]);
           
-          sum += s[i];
+        }
+        
+        // sum up z-values
+        long sum = 0;
+        for (int i = 0; i < length; i ++) {
+          
+          sum += z[i];
         }
         
         // show output
