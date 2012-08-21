237d34cc0ba0d7826621596985cd7d68c38bfd09
Róbert Ormándi <ormandi.robert@gmail.com>
2012-08-21 16:26:28 +0200

    Almost good solution applying strong KMP failure function, but it cannot be finished. The solution is the application of the Z-algorithm.

--- src/com/interviewstreet/stringSimilarity/Solution.java
+++ src/com/interviewstreet/stringSimilarity/Solution.java
@@ -19,35 +19,46 @@ public class Solution {
         // process text
         final int length = text.length();
         
-        // s[i] stores the length of the proper substring ended in i which matches the prefix of the text (KMP)
-        int[] s = new int[length];
-        int[] z = new int[length];
-        
-        // initialize the base case
+        // s[i] stores the length of the proper substring ended in position i which matches the prefix of the text
+        int[] s = new int[length];  // weak KMP failure function
         s[0] = 0;
+        int[] ss = new int[length]; // strong KMP failure function i.e. it's also required t[i+1] != t[s[i]]+1
+        ss[0] = 0;
+        int[] z = new int[length];  // z-values
         z[0] = length;
-        // similar processing than that was proposed in the KMP algorithm
+        
+        // computing failure function of weak and strong KMP, then the corresponding z-values
         for (int i = 1; i < length; i ++) {
+          // weak KMP computation
           int x = i - 1;
           while (0 < x && text.charAt(s[x]) != text.charAt(i)) {
             x = s[x] - 1;
           }
-          s[i] = ( 0 <= x && text.charAt(s[x]) == text.charAt(i)) ? s[x] + 1 : 0;
+          s[i] = ( 0 <= x && text.charAt(s[x]) == text.charAt(i) ) ? s[x] + 1 : 0;
+          
+          // strong KMP computation
+          x = i;
+          while (0 < x && i+1 < length && text.charAt(s[x]) == text.charAt(i+1)) {
+            x = s[x] - 1;
+          }
+          ss[i] = (0 <= x && (length-1 == i || text.charAt(s[x]) != text.charAt(i+1))) ? s[x]: 0;
           
           // compute z-values
-          final int j = i - s[i] + 1;
-          if (0 <= j) {
-            z[j] = s[i];
-          }
-          System.out.println("s[" + i + "] = " + s[i] + ", z[" + j + "] = " + z[j]);
+          final int j = i - ss[i] + 1;
+          z[j] = ss[i];
           
+          // DEBUG:
+          System.out.println("s[" + i + "]=" + s[i] + "\tss[" + i + "] = " + ss[i] + "\tz[" + j + "]=" + z[j]);
         }
         
-        // sum up z-values
+        // compute final solution
         long sum = 0;
-        for (int i = 0; i < length; i ++) {
-          
+        for (int i = 0; i < length; i++) {
+          // sum up z-values
           sum += z[i];
+          
+          // DEBUG
+          System.out.println("z[" + i + "]=" + z[i]);
         }
         
         // show output
