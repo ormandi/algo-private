03a1096d675fd0c7b42c1fd59f0e8bd40992b755
Robert Ormandi <rormandi@evernote.com>
2014-05-06 19:47:42 -0700

    Stock linear unfinished

--- src/com/hackerrank/stockMaximize/Solution.java
+++ src/com/hackerrank/stockMaximize/Solution.java
@@ -7,47 +7,34 @@ public class Solution {
   private static class Node {
     private final int opt;
     private final int sum;
-    private Node(int opt, int sum) {
+    private final int counter;
+    private Node(int opt, int sum, int counter) {
       this.opt = opt;
       this.sum = sum;
+      this.counter = counter;
     }
   }
 
   public static void main(String[] args) throws Exception {
     final BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     final int TESTS = Integer.parseInt(in.readLine());
-    final int N = 12000;
-    final Node[][] solution = new Node[N][];
-    for (int i = 0; i < N; i++) {
-      solution[i] = new Node[i + 1];  // i<=j solution[j][i] = opt[i..j]
-    }
+    final int N = 50000;
+    final Node[] solution = new Node[N];
+    final int[] input = new int[N];
 
     for (int testCase = 0; testCase < TESTS; testCase++) {
       // reads the current testcase
       final int n = Integer.parseInt(in.readLine());
       final String[] line = in.readLine().split("\\s+");
       for (int i = 0; i < n; i++) {
-        solution[i][i] = new Node(0, Integer.parseInt(line[i]));
+        input[i] = Integer.parseInt(line[i]);
       }
 
       // computes the dynamic table
-      for (int diff = 1; diff < n; diff++) {
-        for (int i = 0; i + diff < n; i++) {
-          // computes opt[i..i+diff]
-          int max = solution[i + diff][i + diff].sum * diff - solution[i + diff - 1][i].sum;
-          for (int k = i; k + 1 <= i + diff; k++) {
-            // examines opt[i..k] + opt[k+1..i+diff]
-            final int value = solution[k][i].opt + solution[i + diff][k + 1].opt;
-            if (value > max) {
-              max = value;
-            }
-          }
-          solution[i + diff][i] = new Node(max, solution[i + diff - 1][i].sum + solution[i + diff][i + diff].sum);
-        }
-      }
+      solution[0] = new Node(0, input[0], 1);
+      for (int i = 1;, i < n; i++) {
 
-      // shows the optimal solution
-      System.out.println(solution[n - 1][0].opt);
+      }
     }
     in.close();
   }
